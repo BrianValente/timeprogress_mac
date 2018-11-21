8ab86b56eaefd2b1f8f21c8c84ac262b7907a6fa
Brian Valente <briannvalente@gmail.com>
2018-11-20 22:30:51 -0300

    Add Day, Week, and Month options

--- Year Progress/AppDelegate.swift
+++ Year Progress/AppDelegate.swift
@@ -19,6 +19,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var statusBarButton: NSStatusBarButton!
     var preferencesWindowController: NSWindowController?
     var progressBar: ProgressBar!
+    
+    var dayMenuItem: NSMenuItem!
+    var weekMenuItem: NSMenuItem!
+    var monthMenuItem: NSMenuItem!
+    var yearMenuItem: NSMenuItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         AppDelegate.shared = self
@@ -47,21 +52,63 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             
             //button.title = "        Year: \(getYearProgressPercentage())%"
             button.font = NSFont(name: "San Francisco", size: 1)
-            button.action = #selector(onClick)
+            //button.action = #selector(onClick)
             
             updatePercentage(nil)
             
-            Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(updatePercentage), userInfo: nil, repeats: true)
+            Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updatePercentage), userInfo: nil, repeats: true)
+            
+            addMenu()
         }
     }
     
     @objc func updatePercentage(_ sender: Any?) {
-        statusBarButton.title = "         Year: \(getYearProgressPercentage())%"
+        let value = getCurrentModeValue()
+        statusBarButton.title = "         \(getCurrentModeName()): \(value)%"
+        progressBar.progress = CGFloat(value) / 100
     }
     
     @objc func onClick() {
         openWindow()
         //exit(0)
+    }
+    
+    func getCurrentModeName() -> String {
+        switch getCurrentMode() {
+        case "day":
+            return "Day"
+        case "week":
+            return "Week"
+        case "month":
+            return "Month"
+        case "year":
+            return "Year"
+        default:
+            return ""
+        }
+    }
+    
+    func getCurrentModeValue() -> Int {
+        switch getCurrentMode() {
+        case "day":
+            return getDayProgressPercentage()
+        case "week":
+            return getWeekProgressPercentage()
+        case "month":
+            return getMonthProgressPercentage()
+        case "year":
+            return getYearProgressPercentage()
+        default:
+            return 4
+        }
+    }
+    
+    func getCurrentMode() -> String {
+        if let mode = UserDefaults.standard.string(forKey: "mode") {
+            return mode
+        } else {
+            return "year"
+        }
     }
     
     func openWindow() {
@@ -79,6 +126,69 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 window.makeKeyAndOrderFront(nil)
             }
         }
+    }
+    
+    @objc func openPreferences(_ sender: Any?) {
+        if let window = preferencesWindowController?.window {
+            if window.isVisible {
+                //if (!window.isKeyWindow) {
+                NSApp.activate(ignoringOtherApps: true)
+                window.makeKeyAndOrderFront(nil)
+            } else {
+                preferencesWindowController?.showWindow(self)
+                NSApp.activate(ignoringOtherApps: true)
+                window.makeKeyAndOrderFront(nil)
+            }
+        }
+    }
+    
+    @objc func setMode(_ sender: NSMenuItem) {
+        UserDefaults.standard.setValue(sender.identifier?.rawValue, forKey: "mode")
+        
+        dayMenuItem.state = .off
+        weekMenuItem.state = .off
+        monthMenuItem.state = .off
+        yearMenuItem.state = .off
+        
+        sender.state = .on
+    }
+    
+    func addMenu() {
+        let menu = NSMenu()
+        
+        dayMenuItem = NSMenuItem(title: "Day", action: #selector(setMode), keyEquivalent: "");
+        weekMenuItem = NSMenuItem(title: "Week", action: #selector(setMode), keyEquivalent: "");
+        monthMenuItem = NSMenuItem(title: "Month", action: #selector(setMode), keyEquivalent: "");
+        yearMenuItem = NSMenuItem(title: "Year", action: #selector(setMode), keyEquivalent: "");
+        
+        dayMenuItem.identifier = NSUserInterfaceItemIdentifier("day")
+        weekMenuItem.identifier = NSUserInterfaceItemIdentifier("week")
+        monthMenuItem.identifier = NSUserInterfaceItemIdentifier("month")
+        yearMenuItem.identifier = NSUserInterfaceItemIdentifier("year")
+        
+        switch getCurrentMode() {
+        case "day":
+            dayMenuItem.state = .on
+        case "week":
+            weekMenuItem.state = .on
+        case "month":
+            monthMenuItem.state = .on
+        case "year":
+            yearMenuItem.state = .on
+        default:
+            break
+        }
+        
+        menu.addItem(dayMenuItem)
+        menu.addItem(weekMenuItem)
+        menu.addItem(monthMenuItem)
+        menu.addItem(yearMenuItem)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Preferences", action: #selector(openPreferences), keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
+        
+        statusItem.menu = menu
     }
     
     func changeProgressBarColor(color: String) {
@@ -137,6 +247,48 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let currentDate = date.timeIntervalSince1970
         
         let percentage = ((currentDate - startDate) * 100) / (endDate - startDate)
+        
+        return Int(percentage)
+    }
+    
+    func getMonthProgressPercentage() -> Int {
+        let date = Date()
+        let start = date.startOfMonth().timeIntervalSince1970
+        let end = date.endOfMonth().timeIntervalSince1970
+        let now = date.timeIntervalSince1970
+        print(start, end, now)
+
+        
+        let percentage = ((end - now) * 100) / (end - start)
+        
+        return Int(percentage)
+    }
+    
+    func getWeekProgressPercentage() -> Int {
+        let date = Date()
+        let monday = date.previous(.monday).timeIntervalSince1970
+        let sunday = date.next(.sunday).timeIntervalSince1970
+        let now = date.timeIntervalSince1970
+        
+        let percentage = ((now - monday) * 100) / (sunday - now)
+        
+        return Int(percentage)
+    }
+    
+    func getDayProgressPercentage() -> Int {
+        let date = Date()
+        let calendar = Calendar.current
+        let year = calendar.component(Calendar.Component.year, from: date)
+        let month = calendar.component(Calendar.Component.month, from: date)
+        let day = calendar.component(Calendar.Component.day, from: date)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let today = formatter.date(from: "\(year)-\(month)-\(day) 00:00:00")!.timeIntervalSince1970
+        let tomorrow = today + 86400
+        let now = date.timeIntervalSince1970
+        
+        let percentage = ((now - today) * 100) / (tomorrow - today)
         
         return Int(percentage)
     }
---
+++ Year Progress/DateExtensions.swift
@@ -0,0 +1,102 @@
+//
+//  DateExtensions.swift
+//  Year Progress
+//
+//  Created by Brian Valente on 11/20/18.
+//  Copyright © 2018 Brian Valente. All rights reserved.
+//
+
+import Foundation
+
+
+// https://stackoverflow.com/questions/33397101/how-to-get-mondays-date-of-the-current-week-in-swift
+extension Date {
+    
+    static func today() -> Date {
+        return Date()
+    }
+    
+    func next(_ weekday: Weekday, considerToday: Bool = false) -> Date {
+        return get(.Next,
+                   weekday,
+                   considerToday: considerToday)
+    }
+    
+    func previous(_ weekday: Weekday, considerToday: Bool = false) -> Date {
+        return get(.Previous,
+                   weekday,
+                   considerToday: considerToday)
+    }
+    
+    func get(_ direction: SearchDirection,
+             _ weekDay: Weekday,
+             considerToday consider: Bool = false) -> Date {
+        
+        let dayName = weekDay.rawValue
+        
+        let weekdaysName = getWeekDaysInEnglish().map { $0.lowercased() }
+        
+        assert(weekdaysName.contains(dayName), "weekday symbol should be in form \(weekdaysName)")
+        
+        let searchWeekdayIndex = weekdaysName.index(of: dayName)! + 1
+        
+        let calendar = Calendar(identifier: .gregorian)
+        
+        if consider && calendar.component(.weekday, from: self) == searchWeekdayIndex {
+            return self
+        }
+        
+        var nextDateComponent = DateComponents()
+        nextDateComponent.weekday = searchWeekdayIndex
+        
+        
+        let date = calendar.nextDate(after: self,
+                                     matching: nextDateComponent,
+                                     matchingPolicy: .nextTime,
+                                     direction: direction.calendarSearchDirection)
+        
+        return date!
+    }
+    
+}
+
+// MARK: Helper methods
+extension Date {
+    func getWeekDaysInEnglish() -> [String] {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.locale = Locale(identifier: "en_US_POSIX")
+        return calendar.weekdaySymbols
+    }
+    
+    enum Weekday: String {
+        case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+    }
+    
+    enum SearchDirection {
+        case Next
+        case Previous
+        
+        var calendarSearchDirection: Calendar.SearchDirection {
+            switch self {
+            case .Next:
+                return .forward
+            case .Previous:
+                return .backward
+            }
+        }
+    }
+}
+
+
+
+// https://stackoverflow.com/questions/33605816/first-and-last-day-of-the-current-month-in-swift
+
+extension Date {
+    func startOfMonth() -> Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
+    }
+    
+    func endOfMonth() -> Date {
+        return Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startOfMonth())!
+    }
+}
